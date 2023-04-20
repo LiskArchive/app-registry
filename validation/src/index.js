9cada906270e45f9c98d7c5ca5a292d9a7a99d1e
Vardan Nadkarni <vardan.nadkarni@gmail.com>
2023-04-20 16:54:43 +0530

    Updates validation to accept change files instead of dir

--- validation/src/index.js
+++ validation/src/index.js
@@ -17,50 +17,38 @@ const { validateAllSchemas } = require('./schemaValidation');
 const { validateURLs } = require('./validateURLs');
 const { validateAllWhitelistedFiles } = require('./validateWhitelistedFiles');
 const { validateAllConfigFiles } = require('./validateConfigFiles');
-const { getNetworkDirs, getNestedFilesByName } = require('./utils/fs');
+const { getNetworkDirs } = require('./utils/fs');
 const config = require('../config');
 
 const validate = async () => {
 	// Get all network dirs for schema validation
-	let allNetworkDirs = await getNetworkDirs(config.rootDir);
+	const allNetworkDirs = await getNetworkDirs(config.rootDir);
 
-	// Get all app.json and nativetokens.json files
-	let files = [];
-
-	// Check if user spcified all files changed to validate
+	// Get all modified files
 	const filesChanged = process.argv.slice(2);
-	if (filesChanged.length > 0) {
-		// Get all network dirs changed
-		const uniqueDirs = new Set();
-		for (let i = 0; i < filesChanged.length; i++) {
-			const firstDir = filesChanged[i].split('/')[0];
-			uniqueDirs.add(firstDir);
-		}
-		const changedDirs = [...uniqueDirs];
 
-		// Include only network dirs changed
-		allNetworkDirs = allNetworkDirs.filter(network => changedDirs.some(dir => network.includes(dir)));
-
-		// Filter all changed app.json and nativetokens.json files
-		files = filesChanged.filter(
-			(fileName) => (path.basename(fileName) === config.filename.APP_JSON || path.basename(fileName) === config.filename.NATIVE_TOKENS))
-			.map((fileName) => path.join(config.rootDir, fileName));
-	} else {
-		for (let i = 0; i < allNetworkDirs.length; i++) {
-			/* eslint-disable no-await-in-loop */
-			files.push(...await getNestedFilesByName(allNetworkDirs[i], Object.values(config.filename)));
-			/* eslint-enable no-await-in-loop */
-		}
+	// Get all network dirs changed
+	const uniqueDirs = new Set();
+	for (let i = 0; i < filesChanged.length; i++) {
+		const firstDir = filesChanged[i].split('/')[0];
+		uniqueDirs.add(firstDir);
 	}
+	const changedDirs = [...uniqueDirs];
+	const SelectedNetworkDirs = allNetworkDirs.filter(network => changedDirs.some(dir => network.includes(dir)));
+
+	// Filter all changed app.json and nativetokens.json files
+	const files = filesChanged.filter(
+		(fileName) => (path.basename(fileName) === config.filename.APP_JSON || path.basename(fileName) === config.filename.NATIVE_TOKENS))
+		.map((fileName) => path.join(config.rootDir, fileName));
 
 	// Validate schemas
 	await validateAllSchemas(files);
 
 	// Check whitelisted files in all network directories
-	await validateAllWhitelistedFiles(allNetworkDirs);
+	await validateAllWhitelistedFiles(SelectedNetworkDirs);
 
 	// Check for config files in all network directories
-	await validateAllConfigFiles(allNetworkDirs);
+	await validateAllConfigFiles(SelectedNetworkDirs);
 
 	// Validate serviceURLs
 	await validateURLs(files);
